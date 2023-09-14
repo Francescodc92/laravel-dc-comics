bac5bd23a5af093c3461410e23b2003732c42af9
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-09-14 17:36:57 +0200

    add comics config file , add migration

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
-> Esercizio ...
+> Esercizio Laravel DC Comics
 
 ![preview](./.github/preview.png)
 
-## ...
+## oggi create un nuovo progetto Laravel 10 per gestire un archivio di fumetti.
 
 - DESCRIZIONE:
-  - 
+  - Ciao ragazzi,oggi create un nuovo progetto Laravel 10 per gestire un archivio di fumetti.
 
 - MILESTONE 1 :
-  -  
+  -  Tramite gli appositi comandi artisan create un model con relativa migration e un resource controller.
+
+- MILESTONE 2 :
+  -  Iniziate a definire le prime operazioni CRUD con le relative view:
+    - index()
+    - show()
+    - create()
+    - store()
 
 - BONUS
-  - 
+  - creare il seeder per la tabella comics utilizzando il file in allegato.
 
-- CONSIGLI:
-  - 
-
-```javascript CODE
-```
 
 ## Tecnologie
 
---
+++ database/migrations/2023_09_14_144940_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 64);
+            $table->text('description')->nullable();
+            $table->string('thumb', 1024)->nullable();
+            $table->decimal('price',8,2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 100);
+            $table->string('artist', 255);
+            $table->string('writers', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
