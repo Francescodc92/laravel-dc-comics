6299791fd2fa5cff563e1bbd053e12a8c5ae3c25
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-09-16 19:15:07 +0200

    add home blade card visualization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,21 @@
 
 
 @section('main-content')
-<h1>
-  home page
-</h1>
-
-<img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt="">
+<div class="container py-3">
+  <div class="row row-gap-3">
+    @foreach ($comics as $comic)
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card h-100">
+          <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">{{ $comic->title }}</h5>
+            <p class="card-text">{{ Str::limit($comic->description, 200)}}</p>
+            <p class="card-text text-primary fw-bold">Prezzo: {{ $comic->price }}€</p>
+            <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">Dettagli</a>
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+</div>
 @endsection
